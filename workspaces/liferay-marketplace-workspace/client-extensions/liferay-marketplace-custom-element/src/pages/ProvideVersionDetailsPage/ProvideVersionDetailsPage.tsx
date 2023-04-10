cd5875983b52759c300297f8215d848f6bfb4a75
Igor Franca <igor.franca@liferay.com>
2023-04-10 09:35:48 -0300

    LPS-180717 Add subscription to sku, and go back to using product options

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/ProvideVersionDetailsPage/ProvideVersionDetailsPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/ProvideVersionDetailsPage/ProvideVersionDetailsPage.tsx
@@ -42,58 +42,58 @@ export function ProvideVersionDetailsPage({
 		dispatch,
 	] = useAppContext();
 
-	// useEffect(() => {
-	// 	if (!productOptionId) {
-	// 		const makeFetch = async () => {
-	// 			let newOptionId: number;
-	// 			const options = await getOptions();
+	useEffect(() => {
+		if (!productOptionId) {
+			const makeFetch = async () => {
+				let newOptionId: number;
+				const options = await getOptions();
 
-	// 			const trialOption = options.find(({key}) => key === 'trial');
+				const trialOption = options.find(({key}) => key === 'trial');
 
-	// 			if (!optionId && !trialOption) {
-	// 				newOptionId = await postTrialOption();
-	// 			}
-	// 			else {
-	// 				newOptionId = optionId ?? trialOption!.id;
-	// 			}
+				if (!optionId && !trialOption) {
+					newOptionId = await postTrialOption();
+				}
+				else {
+					newOptionId = optionId ?? trialOption!.id;
+				}
 
-	// 			dispatch({
-	// 				payload: {value: newOptionId},
-	// 				type: TYPES.UPDATE_OPTION_ID,
-	// 			});
+				dispatch({
+					payload: {value: newOptionId},
+					type: TYPES.UPDATE_OPTION_ID,
+				});
 
-	// 			const newProductOptionId = await postTrialProductOption(
-	// 				newOptionId,
-	// 				appProductId
-	// 			);
+				const newProductOptionId = await postTrialProductOption(
+					newOptionId,
+					appProductId
+				);
 
-	// 			dispatch({
-	// 				payload: {value: newProductOptionId},
-	// 				type: TYPES.UPDATE_PRODUCT_OPTION_ID,
-	// 			});
+				dispatch({
+					payload: {value: newProductOptionId},
+					type: TYPES.UPDATE_PRODUCT_OPTION_ID,
+				});
 
-	// 			const noOptionId = await postOptionValue(
-	// 				'no',
-	// 				'No',
-	// 				newProductOptionId,
-	// 				0
-	// 			);
-	// 			const yesOptionId = await postOptionValue(
-	// 				'yes',
-	// 				'Yes',
-	// 				newProductOptionId,
-	// 				1
-	// 			);
+				const noOptionId = await postOptionValue(
+					'no',
+					'No',
+					newProductOptionId,
+					0
+				);
+				const yesOptionId = await postOptionValue(
+					'yes',
+					'Yes',
+					newProductOptionId,
+					1
+				);
 
-	// 			dispatch({
-	// 				payload: {noOptionId, yesOptionId},
-	// 				type: TYPES.UPDATE_PRODUCT_OPTION_VALUES_ID,
-	// 			});
-	// 		};
+				dispatch({
+					payload: {noOptionId, yesOptionId},
+					type: TYPES.UPDATE_PRODUCT_OPTION_VALUES_ID,
+				});
+			};
 
-	// 		makeFetch();
-	// 	}
-	// }, []);
+			makeFetch();
+		}
+	}, []);
 
 	return (
 		<div className="provide-version-details-page-container">
@@ -161,17 +161,15 @@ export function ProvideVersionDetailsPage({
 						const response = await createAppSKU({
 							appProductId,
 							body: {
-								sku: createSkuName(appProductId, appVersion),
 								published: true,
 								purchasable: true,
-
-								// skuOptions: [
-								// 	{
-								// 		key: productOptionId,
-								// 		value: optionValuesId.noOptionId,
-								// 	},
-								// ],
-
+								sku: createSkuName(appProductId, appVersion),
+								skuOptions: [
+									{
+										key: productOptionId,
+										value: optionValuesId.noOptionId,
+									},
+								],
 							},
 						});
 
