a3ed6461e5671eabe73afbf1e195986a2f9422c0
Igor Franca <igor.franca@liferay.com>
2023-04-13 10:59:56 -0300

    LPS-180661 Create customer gate modal

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/GetAppPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/GetAppPage.tsx
@@ -1,17 +1,36 @@
 import ClayButton from '@clayui/button';
 import {useState} from 'react';
 
+import {CustomerGateModal} from '../../components/CustomerGateModal/CustomerGateModal';
 import {GetAppModal} from '../../components/GetAppModal/GetAppModal';
+import {Liferay} from '../../liferay/liferay';
 
-export default function GetAppPage() {
+interface GetAppPageProps {
+	userCustomerChecker: boolean;
+}
+
+export default function GetAppPage({userCustomerChecker}: GetAppPageProps) {
+	const {origin} = window.location;
 	const [showModal, setShowModal] = useState(false);
 
 	return (
 		<>
-			<ClayButton onClick={() => setShowModal(true)}>Get App</ClayButton>
-
-			{showModal && (
+			<ClayButton onClick={() => {
+					Liferay.ThemeDisplay.isSignedIn() ?
+					setShowModal(true) : window.location.href=`${origin}/c/portal/login`;
+				}}
+			>
+				Get App
+			</ClayButton>
+			{userCustomerChecker &&
+			showModal ? (
 				<GetAppModal handleClose={() => setShowModal(false)} />
+			) : (
+				showModal && (
+					<CustomerGateModal
+						handleClose={() => setShowModal(false)}
+					/>
+				)
 			)}
 		</>
 	);
