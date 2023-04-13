740787df7f384b541c5e71cc3e9e53e4c629ead0
Igor Franca <igor.franca@liferay.com>
2023-04-13 10:59:56 -0300

    LPS-180661 Create customer gate modal

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/GetAppPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/GetAppPage/GetAppPage.tsx
@@ -1,17 +1,29 @@
 import ClayButton from '@clayui/button';
 import {useState} from 'react';
 
+import {CustomerGateModal} from '../../components/CustomerGateModal/CustomerGateModal';
 import {GetAppModal} from '../../components/GetAppModal/GetAppModal';
+import {Liferay} from '../../liferay/liferay';
 
-export default function GetAppPage() {
+interface GetAppPageProps {
+	userCustomerChecker: boolean;
+}
+export default function GetAppPage({userCustomerChecker}: GetAppPageProps) {
 	const [showModal, setShowModal] = useState(false);
 
 	return (
 		<>
 			<ClayButton onClick={() => setShowModal(true)}>Get App</ClayButton>
-
-			{showModal && (
+			{userCustomerChecker &&
+			Liferay.ThemeDisplay.isSignedIn() &&
+			showModal ? (
 				<GetAppModal handleClose={() => setShowModal(false)} />
+			) : (
+				showModal && (
+					<CustomerGateModal
+						handleClose={() => setShowModal(false)}
+					/>
+				)
 			)}
 		</>
 	);
