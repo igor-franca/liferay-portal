b704ea67dd75b2ea9e4f2230fab75a8153a98db9
Daniel Bonasser <danielbonasser@gmail.com>
2023-05-17 15:45:55 -0300

    LPS-184427 Extract onContinue to it's own component

--- workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedAppsDashboardPage.tsx
+++ workspaces/liferay-marketplace-workspace/client-extensions/liferay-marketplace-custom-element/src/pages/PublishedAppsDashboardPage/PublishedAppsDashboardPage.tsx
@@ -160,12 +160,15 @@ export function PublishedAppsDashboardPage() {
 									channel.name === 'Marketplace Channel'
 							);
 
-						const isApp = product.categories.find((category) => category.name === 'App');
+						const isApp = product.categories.find(
+							(category) => category.name === 'App'
+						);
 
 						if (
+							isApp &&
 							marketPlaceChannel &&
 							product.catalogId === accountCatalogId
-						 && isApp) {
+						) {
 							newAppList.push({
 								catalogId: product.catalogId,
 								externalReferenceCode:
