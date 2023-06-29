9c9d662e09ace0de55a228265b57bb37b3264b61
Daniel Bonasser <danielbonasser@gmail.com>
2023-06-29 10:44:31 -0300

    LPS-186735 Create CustomVerticalBar component

---
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/VerticalBar/CustomVerticalBar.tsx
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {VerticalBar} from '@clayui/core';
+import React, {ReactNode} from 'react';
+
+import './CustomVerticalBar.scss';
+
+interface CustomVerticalBarProps {
+	children: ReactNode;
+	defaultActive: string;
+	panelWidth?: number;
+	panelWidthMax?: number;
+	panelWidthMin?: number;
+	position: 'left' | 'right';
+	resize?: boolean;
+	triggerSideBarAnimation: boolean;
+	verticalBarItems: {
+		title: string;
+	}[];
+}
+
+export function CustomVerticalBar({
+	children,
+	defaultActive,
+	panelWidth = 1000,
+	panelWidthMax = 1200,
+	panelWidthMin = 400,
+	position,
+	resize = true,
+	triggerSideBarAnimation,
+	verticalBarItems,
+}: CustomVerticalBarProps) {
+	let verticalBarClassName;
+
+	if (triggerSideBarAnimation) {
+		if (position === 'left') {
+			verticalBarClassName =
+				'lfr__objects-custom-vertical-bar--left-open';
+		}
+		else {
+			verticalBarClassName =
+				'lfr__objects-custom-vertical-bar--right-open';
+		}
+	}
+	else {
+		if (position === 'left') {
+			verticalBarClassName =
+				'lfr__objects-custom-vertical-bar--left-closed';
+		}
+		else {
+			verticalBarClassName =
+				'lfr__objects-custom-vertical-bar--right-closed';
+		}
+	}
+
+	return (
+		<VerticalBar
+			className={verticalBarClassName}
+			defaultActive={defaultActive}
+			defaultPanelWidth={panelWidth}
+			panelWidthMax={panelWidthMax}
+			panelWidthMin={panelWidthMin}
+			position={position}
+			resize={resize}
+		>
+			<div className="lfr__objects-custom-vertical-bar-content">
+				<VerticalBar.Content items={verticalBarItems}>
+					{(item) => (
+						<VerticalBar.Panel key={item.title}>
+							{children}
+						</VerticalBar.Panel>
+					)}
+				</VerticalBar.Content>
+			</div>
+		</VerticalBar>
+	);
+}
