e07f798bdf1bf0eb4d3787b72804894e43490bf0
Igor Franca <igor.franca@liferay.com>
2023-07-12 18:15:26 -0300

    LPS-190417 Create Base Rect Flow Component

---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/DiagramBuilder/DiagramBuilder.tsx
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ReactFlow, {Background, Controls, MiniMap} from 'react-flow-renderer';
+
+import './DiagramBuilder.scss';
+
+import React from 'react';
+
+function DiagramBuilder() {
+	return (
+		<div className="lfr-objects__object-diagram">
+			<ReactFlow elements={[]} minZoom={0.1}>
+				<Background size={1} />
+
+				<div className="lfr-objects__object-diagram-controls">
+					<Controls showInteractive={false} />
+
+					<MiniMap />
+				</div>
+			</ReactFlow>
+		</div>
+	);
+}
+
+export default DiagramBuilder;
