f0e31b8ef12329ed77e2a6a98b8ad66eabbe3d5e
Igor Franca <igor.franca@liferay.com>
2023-07-24 13:35:16 -0300

    LPS-191018 Create Right Sidebar Definition Node

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/RightSidebar/RightSidebarRoot.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/RightSidebar/RightSidebarRoot.tsx
@@ -1,31 +1,41 @@
-import { CustomVerticalBar } from "@liferay/object-js-components-web";
-import React, {ReactNode} from "react";
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {CustomVerticalBar} from '@liferay/object-js-components-web';
+import React, {ReactNode} from 'react';
 
 import './RightSidebarRoot.scss';
 
 interface IRightSidebarRoot {
-    children: ReactNode;
+	children: ReactNode;
 }
 
-export function RightSideBarRoot({
-    children
-} : IRightSidebarRoot) {
-    return (
-        <CustomVerticalBar
-            defaultActive="objectsModelBuilderRightSidebar"
-            panelWidth={320}
-            position="right"
-            resize={false}
-            triggerSideBarAnimation={true}
-            verticalBarItems={[
-                {
-                    title: 'objectsModelBuilderRightSidebar',
-                },
-            ]}
-        >
-
-            {children}
-
-        </CustomVerticalBar>    
-    )
+export function RightSideBarRoot({children}: IRightSidebarRoot) {
+	return (
+		<CustomVerticalBar
+			defaultActive="objectsModelBuilderRightSidebar"
+			panelWidth={320}
+			position="right"
+			resize={false}
+			triggerSideBarAnimation={true}
+			verticalBarItems={[
+				{
+					title: 'objectsModelBuilderRightSidebar',
+				},
+			]}
+		>
+			{children}
+		</CustomVerticalBar>
+	);
 }
--- modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ModelBuilder/RightSidebar/index.d.ts
+++ modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ModelBuilder/RightSidebar/index.d.ts
@@ -1,6 +1,22 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {RightSidebarDefinitionNode} from './RightSidebarDefinitionNode';
 import {RightSidebarEmpty} from './RightSidebarEmpty';
 import {RightSideBarRoot} from './RightSidebarRoot';
 export declare const RightSideBar: {
+	DefinitionNode: typeof RightSidebarDefinitionNode;
 	Empty: typeof RightSidebarEmpty;
 	Root: typeof RightSideBarRoot;
 };
