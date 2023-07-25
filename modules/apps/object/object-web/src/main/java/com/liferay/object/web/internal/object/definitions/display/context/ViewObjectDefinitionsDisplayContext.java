00119ddd48894e5ea9f16ded71f4496f05dcc525
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-07-25 20:13:07 -0300

    LPS-191513 Add new parameter to pass objectFolder permission URL

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ViewObjectDefinitionsDisplayContext.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ViewObjectDefinitionsDisplayContext.java
@@ -113,7 +113,8 @@ public class ViewObjectDefinitionsDisplayContext {
 				LanguageUtil.get(_objectRequestHelper.getRequest(), "move"),
 				"update", "update", null),
 			new FDSActionDropdownItem(
-				_getPermissionsURL(), "password-policies", "permissions",
+				getPermissionsURL(ObjectDefinition.class.getName()),
+				"password-policies", "permissions",
 				LanguageUtil.get(
 					_objectRequestHelper.getRequest(), "permissions"),
 				"get", "permissions", "modal-permissions"),
@@ -131,6 +132,35 @@ public class ViewObjectDefinitionsDisplayContext {
 				"label"
 			).build()
 		).build();
+	}
+
+	public String getPermissionsURL(String modelResource) throws Exception {
+		PortletURL portletURL = PortletURLBuilder.create(
+			PortalUtil.getControlPanelPortletURL(
+				_objectRequestHelper.getRequest(),
+				"com_liferay_portlet_configuration_web_portlet_" +
+					"PortletConfigurationPortlet",
+				ActionRequest.RENDER_PHASE)
+		).setMVCPath(
+			"/edit_permissions.jsp"
+		).setRedirect(
+			_objectRequestHelper.getCurrentURL()
+		).setParameter(
+			"modelResource", modelResource
+		).setParameter(
+			"modelResourceDescription", "{name}"
+		).setParameter(
+			"resourcePrimKey", "{id}"
+		).buildPortletURL();
+
+		try {
+			portletURL.setWindowState(LiferayWindowState.POP_UP);
+		}
+		catch (WindowStateException windowStateException) {
+			throw new PortalException(windowStateException);
+		}
+
+		return portletURL.toString();
 	}
 
 	public PortletURL getPortletURL() throws PortletException {
@@ -152,35 +182,6 @@ public class ViewObjectDefinitionsDisplayContext {
 			).put(
 				"type", objectEntryManager.getStorageType()
 			));
-	}
-
-	private String _getPermissionsURL() throws Exception {
-		PortletURL portletURL = PortletURLBuilder.create(
-			PortalUtil.getControlPanelPortletURL(
-				_objectRequestHelper.getRequest(),
-				"com_liferay_portlet_configuration_web_portlet_" +
-					"PortletConfigurationPortlet",
-				ActionRequest.RENDER_PHASE)
-		).setMVCPath(
-			"/edit_permissions.jsp"
-		).setRedirect(
-			_objectRequestHelper.getCurrentURL()
-		).setParameter(
-			"modelResource", ObjectDefinition.class.getName()
-		).setParameter(
-			"modelResourceDescription", "{name}"
-		).setParameter(
-			"resourcePrimKey", "{id}"
-		).buildPortletURL();
-
-		try {
-			portletURL.setWindowState(LiferayWindowState.POP_UP);
-		}
-		catch (WindowStateException windowStateException) {
-			throw new PortalException(windowStateException);
-		}
-
-		return portletURL.toString();
 	}
 
 	private boolean _hasAddObjectDefinitionPermission() {
