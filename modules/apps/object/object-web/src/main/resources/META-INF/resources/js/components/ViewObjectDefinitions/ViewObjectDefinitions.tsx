2b964a7ef811e5dae2f4f7f096a77462513c0333
Igor Franca <igor.franca@liferay.com>
2023-07-25 18:53:21 -0300

    Just for test

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/ViewObjectDefinitions.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/ViewObjectDefinitions.tsx
@@ -22,6 +22,7 @@ import {ModalDeleteObjectDefinition} from './ModalDeleteObjectDefinition';
 import {deleteObjectDefinition} from './objectDefinitionUtil';
 
 interface ViewObjectDefinitionsProps extends IFDSTableProps {
+	editObjectFolderURL: string;
 	baseResourceURL: string;
 	storages: LabelTypeObject[];
 }
@@ -41,6 +42,7 @@ export default function ViewObjectDefinitions({
 	apiURL,
 	baseResourceURL,
 	creationMenu,
+	editObjectFolderURL,
 	id,
 	items,
 	sorting,
@@ -224,6 +226,7 @@ export default function ViewObjectDefinitions({
 			{showModal.addObjectDefinition && (
 				<ModalAddObjectDefinition
 					apiURL={apiURL as string}
+					editObjectFolderURL={editObjectFolderURL}
 					handleOnClose={() => {
 						setShowModal(
 							(previousState: ViewObjectDefinitionsModals) => ({
