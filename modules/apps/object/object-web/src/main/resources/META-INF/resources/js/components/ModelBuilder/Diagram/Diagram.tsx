d3fe413dbf5c2383593770ec3b1b27df8104b5d0
Daniel Bonasser <danielbonasser@gmail.com>
2023-07-26 14:36:06 -0300

    LPS-191846 Upgrade React Flow to V10.3.17

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/Diagram/Diagram.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/Diagram/Diagram.tsx
@@ -3,104 +3,103 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import React from 'react';
 import ReactFlow, {Background, Controls, MiniMap} from 'react-flow-renderer';
 
 import {DefinitionNode} from '../DefinitionNode/DefinitionNode';
 
 import './Diagram.scss';
 
-import React from 'react';
+const NODE_TYPES = {
+	definition: DefinitionNode,
+};
+
+const INITIAL_NODES = [
+	{
+		data: {
+			hasDeleteResourcePermission: true,
+			hasManagePermissionsResourcePermission: true,
+			hasObjectDefinitionPublished: false,
+			isLinkedNode: true,
+			nodeSelected: true,
+			objectDefinitionLabel: 'Postal Address',
+			objectDefinitionName: 'portalAddress',
+			objectFields: [
+				{
+					businessType: 'LongInteger',
+					label: 'ID',
+					name: 'id',
+					primaryKey: true,
+					selected: false,
+				},
+				{
+					businessType: 'Text',
+					label: 'External Reference Code',
+					name: 'erc',
+					primaryKey: false,
+					selected: true,
+				},
+				{
+					businessType: 'Text',
+					label: 'Name',
+					name: 'name',
+					primaryKey: false,
+					selected: false,
+				},
+				{
+					businessType: 'Text',
+					label: 'Street 1',
+					name: 'street1',
+					primaryKey: false,
+					selected: false,
+				},
+				{
+					businessType: 'Text',
+					label: 'Author',
+					name: 'author',
+					primaryKey: false,
+					selected: false,
+				},
+				{
+					businessType: 'Date',
+					label: 'Create Date',
+					name: 'createDate',
+					primaryKey: false,
+					selected: false,
+				},
+				{
+					businessType: 'Date',
+					label: 'Modified Date',
+					name: 'modifiedDate',
+					primaryKey: false,
+					selected: false,
+				},
+				{
+					businessType: 'Text',
+					label: 'Status',
+					name: 'status',
+					primaryKey: false,
+					selected: false,
+				},
+			],
+			system: false,
+		},
+		id: 'A',
+		position: {
+			x: 450,
+			y: 370,
+		},
+		type: 'definition',
+	},
+];
 
 function DiagramBuilder() {
-	const NODE_TYPES = {
-		square: DefinitionNode,
-	};
-
-	const INITIAL_NODES = [
-		{
-			data: {
-				hasDeleteResourcePermission: true,
-				hasManagePermissionsResourcePermission: true,
-				hasObjectDefinitionPublished: false,
-				isLinkedNode: true,
-				nodeSelected: true,
-				objectDefinitionLabel: 'Postal Address',
-				objectDefinitionName: 'portalAddress',
-				objectFields: [
-					{
-						businessType: 'LongInteger',
-						label: 'ID',
-						name: 'id',
-						primaryKey: true,
-						selected: false,
-					},
-					{
-						businessType: 'Text',
-						label: 'External Reference Code',
-						name: 'erc',
-						primaryKey: false,
-						selected: true,
-					},
-					{
-						businessType: 'Text',
-						label: 'Name',
-						name: 'name',
-						primaryKey: false,
-						selected: false,
-					},
-					{
-						businessType: 'Text',
-						label: 'Street 1',
-						name: 'street1',
-						primaryKey: false,
-						selected: false,
-					},
-					{
-						businessType: 'Text',
-						label: 'Author',
-						name: 'author',
-						primaryKey: false,
-						selected: false,
-					},
-					{
-						businessType: 'Date',
-						label: 'Create Date',
-						name: 'createDate',
-						primaryKey: false,
-						selected: false,
-					},
-					{
-						businessType: 'Date',
-						label: 'Modified Date',
-						name: 'modifiedDate',
-						primaryKey: false,
-						selected: false,
-					},
-					{
-						businessType: 'Text',
-						label: 'Status',
-						name: 'status',
-						primaryKey: false,
-						selected: false,
-					},
-				],
-				system: false,
-			},
-			id: 'A',
-			position: {
-				x: 450,
-				y: 370,
-			},
-			type: 'square',
-		},
-	];
-
 	return (
 		<div className="lfr-objects__model-builder-diagram-area">
 			<ReactFlow
-				elements={INITIAL_NODES}
 				minZoom={0.1}
 				nodeTypes={NODE_TYPES}
+				nodes={INITIAL_NODES}
 			>
 				<Background size={1} />
 
