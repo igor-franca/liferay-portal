443867f47806d3115bd09c4642d1a437e31dcff1
Aline Cantarelli <aline.cantarelli@liferay.com>
2023-08-18 11:18:31 -0300

    LPS-192523 Add new object definition and move definition to current folder

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ModelBuilderContext/objectFolderReducer.ts
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/ModelBuilderContext/objectFolderReducer.ts
@@ -4,7 +4,7 @@
  */
 
 import {getLocalizableLabel} from '@liferay/object-js-components-web';
-import {Edge, Node} from 'react-flow-renderer';
+import {Edge, Node, useStore} from 'react-flow-renderer';
 
 import {defaultLanguageId} from '../../../utils/constants';
 import {manyMarkerId} from '../Edges/ManyMarkerEnd';
@@ -24,9 +24,149 @@ import {
 } from './objectFolderReducerUtil';
 import {TYPES} from './typesEnum';
 
-export function objectFolderReducer(state: TState, action: TAction) {
+export function ObjectFolderReducer(state: TState, action: TAction) {
+	const store = useStore();
+
 	switch (action.type) {
+		case TYPES.ADD_NEW_NODE_TO_FOLDER: {
+			const {newObjectDefinition, selectedFolderName} = action.payload;
+			const {nodes} = store.getState();
+			const {
+				editObjectDefinitionURL,
+				elements,
+				leftSidebarItems,
+				objectDefinitionPermissionsURL,
+			} = state;
+			let newPosition = {
+				x: 2 * 300,
+				y: 2 * 400,
+			};
+			if (nodes.length) {
+				const yPositions = nodes.map((node) => node.position.y);
+				const maximumY = Math.max(...yPositions);
+				const maximumNodesYPosition = nodes.filter(
+					(node) => node.position.y === maximumY
+				);
+				const xPositions = maximumNodesYPosition.map(
+					(node) => node.position.x
+				);
+				const maximumX = Math.max(...xPositions);
+				const mostBottomRightNodePosition = maximumNodesYPosition.find(
+					(node) => node.position.x === maximumX
+				)!.position;
+				newPosition = {
+					x: mostBottomRightNodePosition!.x + 300,
+					y: mostBottomRightNodePosition!.y,
+				};
+			}
+			const newLeftSidebarItems = leftSidebarItems.map((item) => {
+				let newDefinition;
+
+				if (item.folderName === selectedFolderName) {
+					newDefinition = {
+						definitionId: newObjectDefinition.id,
+						definitionName: newObjectDefinition.name,
+						name: getLocalizableLabel(
+							newObjectDefinition.defaultLanguageId,
+							newObjectDefinition.label,
+							newObjectDefinition.name
+						),
+						selected: true,
+						type: 'objectDefinition',
+					};
+					const updatedObjectDefinitions = item.objectDefinitions?.map(
+						(objectDefinition) => {
+							return {
+								...objectDefinition,
+								selected: false,
+							};
+						}
+					);
+
+					return {
+						...item,
+						objectDefinitions: [
+							...updatedObjectDefinitions!,
+							newDefinition,
+						],
+					};
+				}
+				else {
+					return {
+						...item,
+					};
+				}
+			}) as LeftSidebarItemType[];
+			const objectFields = newObjectDefinition.objectFields.map(
+				(field) => {
+					return {
+						businessType: field.businessType,
+						externalReferenceCode: field.externalReferenceCode,
+						label: getLocalizableLabel(
+							newObjectDefinition.defaultLanguageId,
+							field.label,
+							field.name
+						),
+						name: field.name,
+						primaryKey: field.name === 'id',
+						required: field.required,
+						selected: false,
+					} as ObjectFieldNode;
+				}
+			);
+			const updatedObjectDefinitionsNodes = elements.map((node) => {
+				return {
+					...node,
+					data: {
+						...node.data,
+						nodeSelected: false,
+					},
+				};
+			});
+			const newNode = {
+				data: {
+					defaultLanguageId: newObjectDefinition.defaultLanguageId,
+					editObjectDefinitionURL,
+					externalReferenceCode:
+						newObjectDefinition.externalReferenceCode,
+					hasObjectDefinitionDeleteResourcePermission:
+						typeof newObjectDefinition.actions.delete !==
+						'undefined',
+					hasObjectDefinitionManagePermissionsResourcePermission:
+						typeof newObjectDefinition.actions.permissions !==
+						'undefined',
+					id: newObjectDefinition.id,
+					isLinkedNode: false, // se clicar no no e linked for true, desabilita os campos da rightsidebar
+					label: getLocalizableLabel(
+						newObjectDefinition.defaultLanguageId!,
+						newObjectDefinition.label,
+						newObjectDefinition.name
+					),
+					name: newObjectDefinition.name,
+					nodeSelected: true,
+					objectDefinitionId: newObjectDefinition.id,
+					objectDefinitionPermissionsURL,
+					objectFields: fieldsCustomSort(objectFields),
+					status: newObjectDefinition.status,
+					system: newObjectDefinition.system,
+				},
+				id: newObjectDefinition.id.toString(),
+				position: newPosition,
+				type: 'objectDefinition',
+			} as Node<ObjectDefinitionNodeData>;
+			const newObjectDefinitionNodes = [
+				...updatedObjectDefinitionsNodes,
+				newNode,
+			] as Node<ObjectDefinitionNodeData>[];
+
+			return {
+				...state,
+				elements: [...newObjectDefinitionNodes],
+				leftSidebarItems: newLeftSidebarItems,
+				selectedDefinitionNode: newNode,
 				showChangesSaved: true,
+			};
+		}
 		case TYPES.CREATE_MODEL_BUILDER_STRUCTURE: {
 			const {objectFolders} = action.payload;
 			const {
@@ -39,6 +179,7 @@ export function objectFolderReducer(state: TState, action: TAction) {
 				const folderDefinitions = folder.definitions?.map(
 					(definition) => {
 						return {
+							definitionId: definition.id,
 							definitionName: definition.name,
 							name: getLocalizableLabel(
 								definition.defaultLanguageId,
@@ -116,10 +257,10 @@ export function objectFolderReducer(state: TState, action: TAction) {
 												type: relationship.type,
 											},
 											id: `reactflow__edge-object-relationship-${relationship.name}-parent-${relationship.objectDefinitionExternalReferenceCode1}-child-${relationship.objectDefinitionExternalReferenceCode2}`,
-											source: `${objectDefinition.name}`,
-											sourceHandle: `${objectDefinition.name}`,
-											target: `${relationship.objectDefinitionName2}`,
-											targetHandle: `${relationship.objectDefinitionName2}`,
+											source: `${objectDefinition.id}`,
+											sourceHandle: `${objectDefinition.id}`,
+											target: `${relationship.objectDefinitionId2}`,
+											targetHandle: `${relationship.objectDefinitionId2}`,
 											type: 'floating',
 										});
 									}
@@ -147,6 +288,7 @@ export function objectFolderReducer(state: TState, action: TAction) {
 								hasObjectDefinitionManagePermissionsResourcePermission:
 									typeof objectDefinition.actions
 										.permissions !== 'undefined',
+								id: objectDefinition.id,
 								isLinkedNode: false,
 								label: getLocalizableLabel(
 									objectDefinition.defaultLanguageId,
@@ -161,7 +303,7 @@ export function objectFolderReducer(state: TState, action: TAction) {
 								status: objectDefinition.status,
 								system: objectDefinition.system,
 							},
-							id: objectDefinition.name,
+							id: objectDefinition.id.toString(),
 							position: {
 								x: positionColumn.x * 300,
 								y: positionColumn.y * 400,
@@ -180,8 +322,47 @@ export function objectFolderReducer(state: TState, action: TAction) {
 				leftSidebarItems: newLeftSidebar,
 			};
 		}
+		case TYPES.DELETE_FOLDER_NODE: {
+			const {currentFolderName, deletedNodeName} = action.payload;
+
+			const {leftSidebarItems} = state;
+
+			let updatedObjectDefinitions;
+
+			const newLeftSidebarItems = leftSidebarItems.map((item) => {
+				if (item.folderName === currentFolderName) {
+					updatedObjectDefinitions = item.objectDefinitions?.filter(
+						(definition) =>
+							definition.definitionName !== deletedNodeName
+					);
+
+					return {
+						...item,
+						objectDefinitions: [...updatedObjectDefinitions!],
+					};
+				}
+				else {
+					return {
+						...item,
+					};
+				}
+			}) as LeftSidebarItemType[];
+
+			return {
+				...state,
+				leftSidebarItems: newLeftSidebarItems,
+			};
+		}
+		case TYPES.SET_ELEMENTS: {
+			const {newElements} = action.payload;
+
+			return {
+				...state,
+				elements: newElements,
+			};
+		}
 		case TYPES.SET_SELECTED_NODE: {
-			const {edges, nodes, selectedObjectDefinitionName} = action.payload;
+			const {edges, nodes, selectedObjectDefinitionId} = action.payload;
 
 			const {leftSidebarItems} = state;
 
@@ -190,8 +371,8 @@ export function objectFolderReducer(state: TState, action: TAction) {
 				data: {
 					...definitionNode.data,
 					nodeSelected:
-						definitionNode.data?.name ===
-						selectedObjectDefinitionName,
+						definitionNode.id ===
+						selectedObjectDefinitionId.toString(),
 				},
 			}));
 
@@ -200,8 +381,8 @@ export function objectFolderReducer(state: TState, action: TAction) {
 					(sidebarDefinition) => ({
 						...sidebarDefinition,
 						selected:
-							selectedObjectDefinitionName ===
-							sidebarDefinition.definitionName,
+							selectedObjectDefinitionId ===
+							sidebarDefinition.definitionId.toString(),
 					})
 				);
 
@@ -218,12 +399,48 @@ export function objectFolderReducer(state: TState, action: TAction) {
 				rightSidebarType: 'objectDefinitionDetails' as RightSidebarType,
 			};
 		}
-		case TYPES.SET_ELEMENTS: {
-			const {newElements} = action.payload;
+		case TYPES.UPDATE_FOLDER_NODE: {
+			const {currentFolderName, updatedNode} = action.payload;
+
+			const {leftSidebarItems} = state;
+
+			let updatedObjectDefinitions;
+
+			const newLeftSidebarItems = leftSidebarItems.map(
+				(item: LeftSidebarItemType) => {
+					if (item.folderName === currentFolderName) {
+						updatedObjectDefinitions = item.objectDefinitions?.map(
+							(definition) => {
+								if (
+									definition.definitionId.toString() ===
+									updatedNode.id?.toString()
+								) {
+									return {
+										...definition,
+										name: updatedNode.label,
+									};
+								}
+
+								return definition;
+							}
+						);
+
+						return {
+							...item,
+							objectDefinitions: [...updatedObjectDefinitions!],
+						};
+					}
+					else {
+						return {
+							...item,
+						};
+					}
+				}
+			) as LeftSidebarItemType[];
 
 			return {
 				...state,
-				elements: newElements,
+				leftSidebarItems: newLeftSidebarItems,
 			};
 		}
 		default:
