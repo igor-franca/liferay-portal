35fdca28ce3830131b698b2c69ae36d7ba6cd9a1
Aline Cantarelli <aline.cantarelli@liferay.com>
2023-08-22 18:37:52 -0300

    LPS-193645 Adapt code to receive linkedDefinition property

--- modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ObjectDetails/ConfigurationContainer.d.ts
+++ modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ObjectDetails/ConfigurationContainer.d.ts
@@ -7,13 +7,13 @@
 
 interface ConfigurationContainerProps {
 	hasUpdateObjectDefinitionPermission: boolean;
-	isLinkedNode?: boolean;
+	linkedDefinition?: boolean;
 	setValues: (values: Partial<ObjectDefinition>) => void;
 	values: Partial<ObjectDefinition>;
 }
 export declare function ConfigurationContainer({
 	hasUpdateObjectDefinitionPermission,
-	isLinkedNode,
+	linkedDefinition,
 	setValues,
 	values,
 }: ConfigurationContainerProps): JSX.Element;
