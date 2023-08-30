24e254386d540113e6d40a60de440f9cf7cf9444
André Massaki Hirai <56045734+andremhirai@users.noreply.github.com>
2023-08-30 15:33:37 -0300

    LPS-185903 | Created the modal and implemented the logic to publish the object definitions

---
+++ modules/apps/object/object-web/types/src/main/resources/META-INF/resources/js/components/ModelBuilder/EditObjectFolderHeader/ModalPublishObjectDefinitions.d.ts
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Observer} from '@clayui/modal/lib/types';
+import React from 'react';
+import {Elements} from 'react-flow-renderer';
+import {ObjectRelationshipEdgeData, TAction} from '../types';
+import './ModalPublishObjectDefinitions.scss';
+interface ModalPublishObjectDefinitionsProps {
+	disableAutoClose: boolean;
+	dispatch: React.Dispatch<TAction>;
+	elements: Elements<ObjectDefinitionNodeData | ObjectRelationshipEdgeData>;
+	observer: Observer;
+	onClose: () => void;
+}
+export declare function ModalPublishObjectDefinitions({
+	disableAutoClose,
+	dispatch,
+	elements,
+	observer,
+	onClose,
+}: ModalPublishObjectDefinitionsProps): JSX.Element;
+export {};
