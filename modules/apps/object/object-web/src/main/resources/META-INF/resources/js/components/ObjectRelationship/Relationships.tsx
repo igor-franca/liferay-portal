0d2425548ebedfbb70dd19f6267a81d7f4761e90
Rebeca Silva <rebeca.silva@liferay.com>
2023-09-20 11:41:05 -0300

    LPS-193646 Trigger the modal to add a relationship using the button on the object definition node

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectRelationship/Relationships.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectRelationship/Relationships.tsx
@@ -228,7 +228,7 @@ export default function Relationships({
 					objectDefinitionExternalReferenceCode={
 						objectDefinitionExternalReferenceCode
 					}
-					parameterRequired={parameterRequired}
+					objectRelationshipParameterRequired={parameterRequired}
 				/>
 			)}
 
