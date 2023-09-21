7114efcb9529713183a4aa615af4853a78fe1013
Igor Franca <igor.franca@liferay.com>
2023-09-21 16:16:32 -0300

    LPS-196919 Remove the Conditions Error Card for your Own Component

---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectValidation/ErrorMessage.tsx
@@ -0,0 +1,81 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Card, RadioField} from '@liferay/object-js-components-web';
+import {InputLocalized} from 'frontend-js-components-web';
+import React from 'react';
+
+import {ObjectValidationErrors} from './useObjectValidationForm';
+
+type ErrorMessageProps = {
+	children: React.ReactNode;
+	disabled: boolean;
+	errors: ObjectValidationErrors;
+	setValidation: (values: Partial<ObjectValidation>) => void;
+	validation: Partial<ObjectValidation>;
+};
+
+export function ErrorMessage({
+	children,
+	disabled,
+	errors,
+	setValidation,
+	validation,
+}: ErrorMessageProps) {
+	const outputValidationTypeArray = [
+		{
+			label: Liferay.Language.get('full-validation-form-summary'),
+			value: 'fullValidation',
+		},
+		{
+			label: Liferay.Language.get('partial-validation-inline-field'),
+			value: 'partialValidation',
+		},
+	];
+
+	return (
+		<Card title={Liferay.Language.get('error-message')}>
+			<InputLocalized
+				disabled={disabled}
+				error={errors.errorLabel}
+				label={Liferay.Language.get('message')}
+				onChange={(errorLabel) => setValidation({errorLabel})}
+				placeholder={Liferay.Language.get('add-an-error-message')}
+				required
+				translations={validation.errorLabel!}
+			/>
+
+			<RadioField
+				defaultValue={validation.outputType}
+				inline={false}
+				label={Liferay.Language.get('output-validation-type')}
+				onChange={(value) => {
+					if (value === 'fullValidation') {
+						setValidation({
+							objectValidationRuleSettings: [],
+							outputType: value as string,
+						});
+
+						return;
+					}
+
+					setValidation({
+						outputType: value as string,
+					});
+				}}
+				options={outputValidationTypeArray}
+				popover={{
+					alignPosition: 'top',
+					content: Liferay.Language.get(
+						'map-the-error-message-to-be-displayed-next-to-the-validated-field'
+					),
+					header: Liferay.Language.get('message-location'),
+				}}
+			/>
+
+			{validation.outputType === 'partialValidation' && children}
+		</Card>
+	);
+}
