75bbf4653c3ccaff0a2f33aa99859cc714b822f0
Mateus Santana <mateus.santana@liferay.com>
2023-09-27 11:42:02 -0300

    LPS-194850 Create updateObjectValidationRuleSettings method

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
@@ -319,8 +319,10 @@ public class ObjectValidationRuleLocalServiceImpl
 			objectValidationRuleId);
 
 		objectValidationRule.setObjectValidationRuleSettings(
-			_addObjectValidationRuleSettings(
-				objectValidationRule, objectValidationRuleSettings));
+			_objectValidationRuleSettingLocalService.
+				updateObjectValidationRuleSettings(
+					objectValidationRuleId, objectValidationRuleSettings,
+					objectValidationRule.getUserId()));
 
 		return objectValidationRule;
 	}
