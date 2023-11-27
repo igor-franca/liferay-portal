8967e07b415a5b67229aa086f1ebdf5494c70be6
Igor Franca <igor.franca@liferay.com>
2023-11-27 15:34:05 -0300

    LPS-201599 Use Usemodal onClose to Avoid Screen Freezing

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModalImportObjectDefinition.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModalImportObjectDefinition.tsx
@@ -334,8 +334,7 @@ export default function ModalImportObjectDefinition({
 	) : warningModalVisible ? (
 		<ModalImportWarning
 			handleImport={() => handleImport(importFormData as FormData)}
-			header={Liferay.Language.get('update-existing-object-definition')}
-			onClose={(value: boolean) => {
+			handleOnClose={(value: boolean) => {
 				if (setModalImportObjectDefinitionInfo) {
 					setModalImportObjectDefinitionInfo(
 						(previousState: ModalImportObjectDefinitionInfo) => ({
@@ -347,6 +346,7 @@ export default function ModalImportObjectDefinition({
 				setWarningModalVisible(false);
 				setImportFormData(undefined);
 			}}
+			header={Liferay.Language.get('update-existing-object-definition')}
 			paragraphs={warningModalBody}
 		/>
 	) : null;
