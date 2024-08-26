3a14dcff683a80dcd405cbfb337b7489c002622e
Igor Franca <igor.franca@liferay.com>
2024-08-26 11:54:46 -0300

    LPD-181879 Use waitFor state visible instead of wait for timeout

--- modules/test/playwright/pages/portal-workflow-kaleo-designer-web/ProcessBuilderPage.ts
+++ modules/test/playwright/pages/portal-workflow-kaleo-designer-web/ProcessBuilderPage.ts
@@ -39,11 +39,9 @@ export class ProcessBuilderPage {
 	async switchToSourceViewAndBackToDiagram() {
 		await this.diagramViewPage.clickSourceViewButton();
 
-		await this.page.waitForTimeout(3000);
-
 		await this.page
-			.getByText('SourceWrite your definition or import a file.')
-			.click();
+			.getByText('xml version	')
+			.waitFor({state: 'visible'});
 
 		await this.sourceViewPage.clickDiagramViewButton();
 	}
