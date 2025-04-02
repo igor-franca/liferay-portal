2597f82272ee4aa9d7bc5e448bf800850efef69b
Igor Franca <igor.franca@liferay.com>
2025-04-02 18:23:36 -0300

    LPD-49643 Ensure that the front end will receive an empty string if there is no translation

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/DDMFormFieldTemplateContextContributorUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/DDMFormFieldTemplateContextContributorUtil.java
@@ -15,6 +15,7 @@ import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -64,7 +65,9 @@ public class DDMFormFieldTemplateContextContributorUtil {
 					String.valueOf(localizedValue.getDefaultLocale())
 				).put(
 					"label",
-					localizedValue.getString(localizedValue.getDefaultLocale())
+					GetterUtil.getString(
+						localizedValue.getString(
+							localizedValue.getDefaultLocale()))
 				).put(
 					"labelMap",
 					() -> {
