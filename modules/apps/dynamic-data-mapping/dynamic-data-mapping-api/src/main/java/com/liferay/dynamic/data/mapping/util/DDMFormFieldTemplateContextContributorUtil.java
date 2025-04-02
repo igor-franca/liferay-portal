ad0c40727903416a49dd0da8081f09a54489d28a
Pedro Leite <pedro.leite@liferay.com>
2025-04-01 22:43:07 -0300

    LPD-49643 Reuse to single select

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/DDMFormFieldTemplateContextContributorUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/DDMFormFieldTemplateContextContributorUtil.java
@@ -29,25 +29,6 @@ import java.util.Map;
  */
 public class DDMFormFieldTemplateContextContributorUtil {
 
-	public static Map<Locale, String> getListTypeEntryNameMap(
-		String key, long listTypeDefinitionId,
-		ListTypeEntryLocalService listTypeEntryLocalService) {
-
-		if (listTypeDefinitionId == 0) {
-			return null;
-		}
-
-		ListTypeEntry listTypeEntry =
-			listTypeEntryLocalService.fetchListTypeEntry(
-				listTypeDefinitionId, key);
-
-		if (listTypeEntry == null) {
-			return null;
-		}
-
-		return listTypeEntry.getNameMap();
-	}
-
 	public static Map<String, Object> getLocaleMap(Locale defaultLocale) {
 		JSONObject localeJSONObject = _getLocaleJSONObject(defaultLocale);
 
@@ -84,8 +65,8 @@ public class DDMFormFieldTemplateContextContributorUtil {
 				).put(
 					"labelMap",
 					() -> {
-						Map<Locale, String> labeMap = getListTypeEntryNameMap(
-							listTypeDefinitionId, optionValue,
+						Map<Locale, String> labeMap = _getListTypeEntryNameMap(
+							optionValue, listTypeDefinitionId,
 							listTypeEntryLocalService);
 
 						if (labeMap != null) {
@@ -127,6 +108,25 @@ public class DDMFormFieldTemplateContextContributorUtil {
 					ddmFormField.getProperty("isLocalizationSupported"));
 			}
 		).build();
+	}
+
+	private static Map<Locale, String> _getListTypeEntryNameMap(
+		String key, long listTypeDefinitionId,
+		ListTypeEntryLocalService listTypeEntryLocalService) {
+
+		if (listTypeDefinitionId == 0) {
+			return null;
+		}
+
+		ListTypeEntry listTypeEntry =
+			listTypeEntryLocalService.fetchListTypeEntry(
+				listTypeDefinitionId, key);
+
+		if (listTypeEntry == null) {
+			return null;
+		}
+
+		return listTypeEntry.getNameMap();
 	}
 
 	private static JSONObject _getLocaleJSONObject(Locale locale) {
