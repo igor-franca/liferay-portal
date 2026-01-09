c64b63f6a6f251144121feb4b5237bc170937836
Pedro Leite <pedro.leite@liferay.com>
2026-01-08 22:54:51 -0300

    LPD-69878 [BE] Add FDSView for tasks

---
+++ modules/apps/site/site-cmp-site-initializer/src/main/java/com/liferay/site/cmp/site/initializer/internal/frontend/data/set/view/table/TaskSectionTableFDSView.java
@@ -0,0 +1,83 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cmp.site.initializer.internal.frontend.data.set.view.table;
+
+import com.liferay.frontend.data.set.constants.FDSTimeZoneBehaviorConstants;
+import com.liferay.frontend.data.set.view.FDSView;
+import com.liferay.frontend.data.set.view.table.BaseTableFDSView;
+import com.liferay.frontend.data.set.view.table.DateTimeFDSTableSchemaField;
+import com.liferay.frontend.data.set.view.table.FDSTableSchema;
+import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
+import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
+import com.liferay.site.cmp.site.initializer.internal.constants.CMPSiteInitializerFDSNames;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@Component(
+	property = "frontend.data.set.name=" + CMPSiteInitializerFDSNames.CMP_TASK,
+	service = FDSView.class
+)
+public class TaskSectionTableFDSView extends BaseTableFDSView {
+
+	@Override
+	public FDSTableSchema getFDSTableSchema(Locale locale) {
+		FDSTableSchemaBuilder fdsTableSchemaBuilder =
+			_fdsTableSchemaBuilderFactory.create();
+
+		return fdsTableSchemaBuilder.add(
+			"embedded.title", "title",
+			fdsTableSchemaField -> fdsTableSchemaField.setActionId(
+				"actionLink"
+			).setContentRenderer(
+				"simpleActionLinkTableCellRenderer"
+			)
+		).add(
+			"embedded.assignTo.name", "assign-to"
+		).add(
+			"embedded.r_cmpProjectToCMPTask_c_cmpProject.title", "project"
+		).add(
+			_addDateFDSTableSchemaField("embedded.dueDate", "due-date")
+		).add(
+			"embedded.state", "state",
+			fdsTableSchemaField -> fdsTableSchemaField.setContentRenderer(
+				"stateTableCellRenderer")
+		).build();
+	}
+
+	private DateTimeFDSTableSchemaField _addDateFDSTableSchemaField(
+		String fieldName, String label) {
+
+		DateTimeFDSTableSchemaField dateFDSTableSchemaField =
+			new DateTimeFDSTableSchemaField();
+
+		dateFDSTableSchemaField.setContentRenderer(
+			"dateTime"
+		).setFieldName(
+			fieldName
+		).setLabel(
+			label
+		).setLocalizeLabel(
+			true
+		).setSortable(
+			true
+		);
+
+		dateFDSTableSchemaField.setTimeZoneBehavior(
+			FDSTimeZoneBehaviorConstants.APPLY_THEME_DISPLAY_TIME_ZONE);
+
+		return dateFDSTableSchemaField;
+	}
+
+	@Reference
+	private FDSTableSchemaBuilderFactory _fdsTableSchemaBuilderFactory;
+
+}
