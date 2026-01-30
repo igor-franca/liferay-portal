da5f65b3d403882ec0d7ef4f1f589c3f99c055fa
Igor Franca <igor.franca@liferay.com>
2026-01-30 09:34:21 -0300

    LPD-75798 Add dataSetId as an optional property of the View

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/utils/types.ts
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/utils/types.ts
@@ -264,6 +264,7 @@ export interface IView {
 	contentRenderer?: string;
 	contentRendererClientExtension?: boolean;
 	contentRendererModuleURL?: string;
+	dataSetId?: string;
 	default?: boolean;
 	label?: string;
 	name?: string;
