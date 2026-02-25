f9ca6dbf499f236a20bb96b34d0623397b4b66f4
Danny Situ <danny.situ@liferay.com>
2026-02-24 16:17:58 -0800

    LPD-78591 Seperate BulkActionType from BulkActionTaskType

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/types/BulkActionTask.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/common/types/BulkActionTask.ts
@@ -73,13 +73,12 @@ export interface IBulkActionTaskStarter {
 	get type(): string;
 }
 
-export interface IBulkActionTaskStarterDTO<
-	T extends keyof IBulkActionTaskType,
-> {
+export interface IBulkActionTaskStarterDTO<T extends keyof IBulkActionType> {
+	additionalData?: Record<string, any>;
 	apiURL?: string;
 	dataSetId?: string;
 	entryClassName?: string;
-	keyValues?: IBulkActionTaskType[T];
+	keyValues?: IBulkActionType[T];
 	onCreateError?:
 		| ((response: RequestResult<IBulkActionTaskPage>) => void)
 		| null;
@@ -89,10 +88,10 @@ export interface IBulkActionTaskStarterDTO<
 	overrideDefaultErrorToast?: boolean;
 	overrideDefaultSuccessToast?: boolean;
 	selectedData: IBulkActionFDSData;
-	type: keyof IBulkActionTaskType;
+	type: keyof IBulkActionType;
 }
 
-export interface IBulkActionTaskType {
+export interface IBulkActionType {
 	AssignStructureDefaultWorkflowBulkAction: {
 		workflow?: string;
 	};
@@ -110,7 +109,9 @@ export interface IBulkActionTaskType {
 	DeleteAssetVersionBulkAction: {
 		versions?: number[];
 	};
-	DeleteBulkAction: {};
+	DeleteBulkAction: {
+		className?: string;
+	};
 	DownloadBulkAction: {};
 	DueDateBulkAction: {
 		dueDate?: string;
@@ -139,6 +140,25 @@ export interface IBulkActionTaskType {
 	};
 }
 
+export interface IBulkActionTaskType {
+	AssignStructureDefaultWorkflowBulkAction: string;
+	AssignToBulkAction: string;
+	CopyBulkAction: string;
+	DefaultPermissionBulkAction: string;
+	DeleteAssetVersionBulkAction: string;
+	DeleteBulkAction: string;
+	DeleteTaskBulkAction: string;
+	DownloadBulkAction: string;
+	DueDateBulkAction: string;
+	ExpireBulkAction: string;
+	KeywordBulkAction: string;
+	MoveBulkAction: string;
+	PermissionBulkAction: string;
+	ResetPermissionBulkAction: string;
+	StatusBulkAction: string;
+	TaxonomyCategoryBulkAction: string;
+}
+
 export type TBulkActionTaskDTO = {
 	bulkActionItems: IBulkActionFDSDataItemTransformed[] | [];
 	selectAll?: IBulkActionFDSData['selectAll'];
@@ -146,6 +166,6 @@ export type TBulkActionTaskDTO = {
 		selectAll: IBulkActionFDSData['selectAll'];
 		[k: string]: any;
 	};
-	type: keyof IBulkActionTaskType;
+	type: keyof IBulkActionType;
 	versions?: number[] | [];
-} & IBulkActionTaskType[keyof IBulkActionTaskType];
+} & IBulkActionType[keyof IBulkActionType];
