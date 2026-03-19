7841e035f66a566fa809d7636dcbc0035ccedf49
Mikel Lorza <mikel.lorza@liferay.com>
2026-03-19 11:56:11 +0100

    LPD-83050 Change name from assetLibraries to candidateAssetLibraries since these asset libraries are filtered by the context, for example, the home of the space or the folder
    (cherry picked from commit 0b44b6260918ea254d078766854f59bca9fbbfea)

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/display/context/BaseSectionDisplayContext.java
@@ -117,11 +117,6 @@ public abstract class BaseSectionDisplayContext {
 
 	public Map<String, Object> getAdditionalProps() {
 		return HashMapBuilder.<String, Object>put(
-			"assetLibraries",
-			_sectionDisplayContextHelper.getDepotEntriesJSONArray(
-				httpServletRequest,
-				getRootObjectEntryFolderExternalReferenceCode())
-		).put(
 			"autocompleteURL",
 			() -> StringBundler.concat(
 				"/o/search/v1.0/search?emptySearch=",
@@ -147,6 +142,11 @@ public abstract class BaseSectionDisplayContext {
 			"brokenLinksCheckerEnabled",
 			GetterUtil.getBoolean(
 				PropsUtil.get(PropsKeys.CMS_BROKEN_LINKS_CHECKER_ENABLED))
+		).put(
+			"candidateAssetLibraries",
+			_sectionDisplayContextHelper.getDepotEntriesJSONArray(
+				httpServletRequest,
+				getRootObjectEntryFolderExternalReferenceCode())
 		).put(
 			"cmsGroupId",
 			() -> {
