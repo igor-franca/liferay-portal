faf662d45e7c5678121d45b84d11cec61fd5fe03
Brian Chan <brian.chan@liferay.com>
2026-04-07 08:04:26 -0300

    LPD-84964 Match 'File name is null'

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/resource/v1_0/test/ObjectEntryResourceTest.java
@@ -19705,7 +19705,7 @@ public class ObjectEntryResourceTest {
 			fileEntry -> JSONUtil.put(
 				"status", "BAD_REQUEST"
 			).put(
-				"title", "File content cannot be empty"
+				"title", "File content is empty"
 			),
 			_toFileEntry(
 				Base64::encode, StringPool.BLANK,
