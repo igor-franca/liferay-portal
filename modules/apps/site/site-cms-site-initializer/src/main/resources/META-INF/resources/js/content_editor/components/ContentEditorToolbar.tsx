a5d8cdb62e11d7a19d0950f6ec63844a606e9c17
Bruno Carvalho <bruno.carvalho@liferay.com>
2026-04-24 12:13:41 -0300

    LPD-86097 Use default-language title in publish confirmation

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorToolbar.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorToolbar.tsx
@@ -64,11 +64,21 @@ export default function ContentEditorToolbar({
 			const form = getForm();
 
 			if (form?.checkValidity?.()) {
-				const titleInput = form.querySelector(
-					'[name^="ObjectField_title"]'
-				) as HTMLInputElement;
+				const defaultLanguageId =
+					Liferay.ThemeDisplay.getDefaultLanguageId();
 
-				const value = titleInput ? titleInput.value : headerTitle;
+				const defaultTitleInput = form.querySelector(
+					`[name="ObjectField_title_${defaultLanguageId}"]`
+				) as HTMLInputElement | null;
+
+				const currentTitleInput = form.querySelector(
+					'[name="ObjectField_title"]'
+				) as HTMLInputElement | null;
+
+				const value =
+					defaultTitleInput?.value ||
+					currentTitleInput?.value ||
+					headerTitle;
 
 				sessionStorage.setItem(
 					'com.liferay.site.cms.site.initializer.successMessage',
